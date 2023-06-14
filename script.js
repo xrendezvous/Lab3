d58416e0ba1813ab8f412d4510f578d144bff489
Aryna Reutska <arina.reutskaya110@gmail.com>
2023-06-14 21:27:54 +0300

    update lab3

--- script.js
+++ script.js
@@ -1,80 +1,93 @@
-// Get the necessary DOM elements
-const inputField = document.querySelector('.product-search');
-const addButton = document.querySelector('.adding-product');
-const productList = document.querySelector('.block-left');
+document.addEventListener('DOMContentLoaded', function() {
+    const addButton = document.querySelector('.adding-product');
+    const inputField = document.querySelector('.product-search');
+    const productList = document.querySelector('.block-left');
 
-// Add event listener to the add button
-addButton.addEventListener('click', () => {
-    // Get the value from the input field
-    const productName = inputField.value;
+    addButton.addEventListener('click', function() {
+        const productName = inputField.value.trim();
+        if (productName !== '') {
+            const newProduct = createProductElement(productName);
+            productList.appendChild(newProduct);
+            inputField.value = '';
+            inputField.focus();
+        }
+    });
 
-    // Create the HTML elements for the new product
-    const productInfo = document.createElement('div');
-    productInfo.className = 'product-info';
+    function createProductElement(name) {
+        const productInfo = document.createElement('div');
+        productInfo.className = 'product-info';
 
-    const productNameContainer = document.createElement('div');
-    productNameContainer.className = 'product-name-container';
+        const productNameContainer = document.createElement('div');
+        productNameContainer.className = 'product-name-container';
 
-    const productNameElement = document.createElement('div');
-    productNameElement.className = 'product-name';
-    productNameElement.textContent = productName;
+        const productNameElement = document.createElement('div');
+        productNameElement.className = 'product-name';
+        productNameElement.innerText = name;
 
-    productNameContainer.appendChild(productNameElement);
-    productInfo.appendChild(productNameContainer);
+        productNameContainer.appendChild(productNameElement);
+        productInfo.appendChild(productNameContainer);
 
-    const productDetails = document.createElement('div');
-    productDetails.className = 'product-details';
+        const productDetails = document.createElement('div');
+        productDetails.className = 'product-details';
 
-    const counter = document.createElement('div');
-    counter.className = 'counter';
+        const counter = document.createElement('div');
+        counter.className = 'counter';
 
-    const minusButton = document.createElement('button');
-    minusButton.className = 'round-button-red';
-    minusButton.type = 'button';
-    minusButton.textContent = '-';
-    minusButton.setAttribute('data-tooltip', 'Мінус');
+        const minusButton = document.createElement('button');
+        minusButton.className = 'round-button-red';
+        minusButton.type = 'button';
+        minusButton.dataset.tooltip = 'Мінус';
+        minusButton.innerText = '-';
 
-    const countDisplay = document.createElement('div');
-    countDisplay.className = 'count-display';
-    countDisplay.textContent = '0';
+        const countDisplay = document.createElement('div');
+        countDisplay.className = 'count-display';
+        countDisplay.style.fontFamily = 'Arial';
+        countDisplay.style.fontSize = 'small';
+        countDisplay.style.fontWeight = 'bold';
+        countDisplay.innerText = '1';
 
-    const plusButton = document.createElement('button');
-    plusButton.className = 'round-button-green';
-    plusButton.type = 'button';
-    plusButton.textContent = '+';
-    plusButton.setAttribute('data-tooltip', 'Плюс');
+        const plusButton = document.createElement('button');
+        plusButton.className = 'round-button-green';
+        plusButton.type = 'button';
+        plusButton.dataset.tooltip = 'Плюс';
+        plusButton.innerText = '+';
 
-    counter.appendChild(minusButton);
-    counter.appendChild(countDisplay);
-    counter.appendChild(plusButton);
+        counter.appendChild(minusButton);
+        counter.appendChild(countDisplay);
+        counter.appendChild(plusButton);
 
-    const buySection = document.createElement('section');
-    buySection.className = 'buy-section';
+        const buySection = document.createElement('section');
+        buySection.className = 'buy-section';
 
-    const boughtButton = document.createElement('button');
-    boughtButton.className = 'button-bought';
-    boughtButton.textContent = 'Куплено';
-    boughtButton.setAttribute('data-tooltip', 'Куплено');
+        const boughtButton = document.createElement('button');
+        boughtButton.className = 'button-bought';
+        boughtButton.style.fontFamily = 'Arial';
+        boughtButton.style.fontSize = 'small';
+        boughtButton.style.fontWeight = 'bold';
+        boughtButton.dataset.tooltip = 'Куплено';
+        boughtButton.innerText = 'Куплено';
 
-    const deleteButton = document.createElement('button');
-    deleteButton.className = 'button-square';
-    deleteButton.textContent = 'х';
-    deleteButton.setAttribute('data-tooltip', 'Видалити товар');
+        const deleteButton = document.createElement('button');
+        deleteButton.className = 'button-square';
+        deleteButton.style.fontFamily = 'Arial';
+        deleteButton.style.fontSize = 'small';
+        deleteButton.style.fontWeight = 'bold';
+        deleteButton.dataset.tooltip = 'Видалити товар';
+        deleteButton.innerText = 'х';
 
-    buySection.appendChild(boughtButton);
-    buySection.appendChild(deleteButton);
+        deleteButton.addEventListener('click', function() {
+            productInfo.remove();
+        });
 
-    productDetails.appendChild(counter);
-    productDetails.appendChild(buySection);
+        buySection.appendChild(boughtButton);
+        buySection.appendChild(deleteButton);
 
-    productInfo.appendChild(productDetails);
+        productDetails.appendChild(counter);
+        productDetails.appendChild(buySection);
 
-    // Add the new product to the product list
-    productList.appendChild(productInfo);
+        productInfo.appendChild(productDetails);
 
-    // Clear the input field
-    inputField.value = '';
-
-    // Set the focus back to the input field
-    inputField.focus();
+        return productInfo;
+    }
 });
+
